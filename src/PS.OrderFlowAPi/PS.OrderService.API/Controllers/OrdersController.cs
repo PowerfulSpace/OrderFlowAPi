55fd7667b143a737870c44931a8dcc2d314b5282
Alexander <sany199574@gmail.com>
2025-04-27 00:05:34 +0300

    Удалён контроллер  -weatherForecastController

---
+++ src/PS.OrderFlowAPi/PS.OrderService.API/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PS.OrderService.Application.Services;
+
+namespace PS.OrderService.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderProcessor _orderService;
+
+        public OrdersController(IOrderProcessor orderService)
+        {
+            _orderService = orderService;
+        }
+    }
+}
